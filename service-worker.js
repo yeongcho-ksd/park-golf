4a9b49c7e181a2746001dbb4be71e2536fbd8dd0
yeongcho-ksd <hwalsarang@gmail.com>
2026-01-10 17:03:33 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
